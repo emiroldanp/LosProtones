4ae1d34f11c92a6efc9a4aaf8858df48d88484ac
javoflores28 <javoflores28@hotmai.com>
2020-05-07 18:33:09 -0500

    LastRegion and limits

---
+++ SpaceSurvivor/Assets/Scripts/AlienMiniShot.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienMiniShot : MonoBehaviour
+{
+    //public Rigidbody2D rb;
+	//public float moveSpeed;
+
+	//public float rangeToChasePlayer;
+	//private Vector3 moveDirection;
+
+	//public Animator anim;
+
+	//public int health = 450;
+
+    private float timeBtwShots;
+
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+
+   
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+        
+    }
+
+    void Update()
+    {
+    /*
+    	if(Vector3.Distance(transform.position, AstronautMovement.instance.transform.position) < rangeToChasePlayer){
+
+    		moveDirection = AstronautMovement.instance.transform.position - transform.position;
+    	} else {
+
+    		moveDirection = Vector3.zero;
+
+
+    	}
+
+        
+
+        moveDirection.Normalize();
+
+        rb.velocity = moveDirection * moveSpeed;
+
+    */
+
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+        } else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+      
+
+
+
+
+    }
+/*
+    public void DamageEnemy(int damage){
+
+    	health -= damage;
+
+    	if(health <= 0){
+
+    		Destroy(gameObject);
+
+    	}
+
+        */
+
+    }
+
