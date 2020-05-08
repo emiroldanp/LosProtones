5fb4b9f2eabda073f01c73ad82f5b2f5ac7243d9
JC <jacosta291@gmail.com>
2020-05-08 17:09:42 -0500

    Added Pause menu

---
+++ SpaceSurvivor/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject PauseUI;
+
+    public GameObject HUD;
+
+    private void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("KeyPress, should pause");
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+       
+    }
+
+    void Resume()
+    {
+        PauseUI.SetActive(false);
+        HUD.SetActive(true);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+
+    }
+
+    void Pause()
+    {
+        PauseUI.SetActive(true);
+        HUD.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
