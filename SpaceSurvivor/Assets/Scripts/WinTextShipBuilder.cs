0bc365d765ced9d086c8ef836ea633e0336cf351
javoflores28 <javoflores28@hotmai.com>
2020-05-27 00:00:30 -0500

    HUD and ShipBuilder scene

---
+++ SpaceSurvivor/Assets/Scripts/WinTextShipBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTextShipBuilder : MonoBehaviour
+{
+    private int pointsToWin;
+    private int currentPoints;
+    public GameObject myShips;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsToWin = myShips.transform.childCount;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentPoints >= pointsToWin){
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void AddPoints(){
+
+        currentPoints++;
+
+    }
+}
